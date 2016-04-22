819ed0e70c808eb91c221af4802627e564343091
Patrick <ammeyjohn@gmail.com>
2016-04-22 21:45:01 +0800

    Rename app.html to index.html; Compile all js file to dist folder.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'todo-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<pre><h1>My First Angular 2 App</h1></pre>'
 })
 export class AppComponent { }
