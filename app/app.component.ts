a9803450d334b6914e03706c8be567874613c934
yuanjie <yuanjie@shanghai3h.com>
2016-04-28 15:12:17 +0800

    Update the style of todo main page.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,24 @@
 import {Component} from 'angular2/core';
-import {NavbarComponent} from './navbar/navbar.component';
 import {TodoComponent} from './todo/todo.component';
 
 @Component({
     selector: 'todo-app',
     templateUrl: 'app/app.template.html',
     directives: [
-    	NavbarComponent,
     	TodoComponent
-    ]
+    ],
+    styles:[`
+		header.app h1 {
+			margin-top: 50px;
+			width: 100%;
+			font-size: 100px;
+			font-weight: 100;
+			text-align: center;
+			color: rgba(175, 47, 47, 0.15);
+			-webkit-text-rendering: optimizeLegibility;
+    		   -moz-text-rendering: optimizeLegibility;
+					text-rendering: optimizeLegibility;
+		}    
+    `]
 })
 export class AppComponent { }
--- app/todo/todo.component.ts
+++ app/todo/todo.component.ts
@@ -6,7 +6,36 @@ import {TodoService} from './todoService';
     selector: 'todo',
     templateUrl: 'app/todo/todo.template.html',
     providers: [ TodoService ],
-    styles: [``]
+    styles: [`
+		section.todo-new {			
+			line-height: 1.4em;
+			background: #fff;
+			color: #4d4d4d;
+			min-width: 230px;
+			max-width: 550px;
+			margin: 50px auto;
+			font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+			font-weight: 300;
+			box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2),
+	            		0 25px 50px 0 rgba(0, 0, 0, 0.1);
+			-webkit-font-smoothing: antialiased;
+			   -moz-font-smoothing: antialiased;
+				    font-smoothing: antialiased;			
+		}
+
+    	section.todo-new input {
+			padding: 16px 16px 16px 30px;
+			border: none;
+			box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
+			width: 100%;
+			font-size: 24px;
+			font-style: italic;
+			line-height: 1.4em;	
+			-webkit-font-smoothing: antialiased;
+			   -moz-font-smoothing: antialiased;
+					font-smoothing: antialiased;	
+    	}
+    `]
 })
 
 export class TodoComponent {
